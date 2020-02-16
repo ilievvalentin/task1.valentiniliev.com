07309aa01fee5d3a3a33272e63b7b147a362f8b8
Valentin Iliev <ilievvalentin@icloud.com>
2020-02-17 00:31:16 +0200

    Initial commit

--- src/app/admin-hyperlink.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AdminHyperlink } from './admin-hyperlink';
-
-describe('AdminHyperlink', () => {
-  it('should create an instance', () => {
-    expect(new AdminHyperlink()).toBeTruthy();
-  });
-});
--- src/app/hyperlink/model/hyperlink.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Hyperlink } from './hyperlink';
-
-describe('Hyperlink', () => {
-  it('should create an instance', () => {
-    expect(new Hyperlink()).toBeTruthy();
-  });
-});
